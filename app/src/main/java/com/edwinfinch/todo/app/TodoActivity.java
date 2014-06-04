55e0356c62978fd5105f3c1c4430e5d2a15b850c
edwinfinch <edwinfinch8@gmail.com>
2014-06-03 22:11:12 -0400

    Initial commit
    0.2 alpha: very basic app. No pebble integration, yet.

---
+++ app/src/main/java/com/edwinfinch/todo/app/TodoActivity.java
@@ -0,0 +1,113 @@
+package com.edwinfinch.todo.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.content.Intent;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.commons.io.FileUtils;
+
+public class TodoActivity extends Activity {
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView lvItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo);
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        items = new ArrayList<String>();
+        readItems();
+        itemsAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        setupListViewListener();
+        setTitle(getString(R.string.title_plus_ver));
+        getActionBar().setIcon(R.drawable.logo);
+        File filesDir = getFilesDir();
+        File f = new File(filesDir, "todo.txt");
+        if(!f.exists()){
+            items.add("Welcome to TurtleNotes (for Pebble)");
+            items.add("To add an item, enter your text in the box below and hit 'Add'");
+            items.add("To delete an item, press and hold on it.");
+            items.add("Try deleting this item.");
+            items.add("To sync with pebble, install the watchapp and hit 'Sync with Pebble'");
+        }
+    }
+
+    private void setupListViewListener(){
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                items.remove(i);
+                itemsAdapter.notifyDataSetChanged();
+                saveItems();
+                setTitle(getString(R.string.deleted));
+                return true;
+            }
+        });
+    }
+    //Test
+
+    private void readItems(){
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try{
+            items = new ArrayList<String>(FileUtils.readLines(todoFile));
+        } catch (IOException e){
+            items = new ArrayList<String>();
+            e.printStackTrace();
+        }
+    }
+
+    private void saveItems(){
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try{
+            FileUtils.writeLines(todoFile, items);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void addTodoItem(View v) {
+        EditText etNewItem = (EditText)
+                findViewById(R.id.etNewItem);
+        itemsAdapter.add(etNewItem.getText().toString());
+        etNewItem.setText("");
+        saveItems();
+        setTitle(getString(R.string.saved));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.todo, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent myIntent = new Intent(TodoActivity.this, AbootActivity.class);
+            TodoActivity.this.startActivity(myIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
